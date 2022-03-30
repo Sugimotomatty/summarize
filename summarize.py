82ed937f905efcd04510adc674d7f81a9d0361fc
shung-ich <takahashi.shunichi.83a@st.kyoto-u.ac.jp>
2022-03-30 18:30:33 +0900

    区切り文字追加, similarity_limit調整

--- summarize.py
+++ summarize.py
@@ -11,9 +11,7 @@ from pysummarization.similarityfilter.tfidf_cosine import TfIdfCosine
 #文字数が長すぎるとNon-UTF-8 code starting with '\xe3'というエラーが起こってしまうので文章を分けて読み込む仕組みが必要
 
 
-
 #要約を行う関数
-
 def summa(document):
 
     # NLPのオブジェクト
@@ -29,14 +27,15 @@ def summa(document):
     similarity_filter.nlp_base = nlp_base
 
     # 類似性がこの値を超えると、文は切り捨てられます
-    similarity_filter.similarity_limit = 0.25
-
-
+    similarity_filter.similarity_limit = 0.5
 
     auto_abstractor = AutoAbstractor()
 
     # トークナイザーを設定します。 これは、MeCabを使用した日本語のトークナイザーです
     auto_abstractor.tokenizable_doc = MeCabTokenizer()
+
+    # 文の切れ目で使われる文字を設定します。    
+    auto_abstractor.delimiter_list = ['．', '。', '\n']
 
     # ドキュメントを抽象化およびフィルタリングするオブジェクト
     abstractable_doc = TopNRankAbstractor()
@@ -46,5 +45,3 @@ def summa(document):
     result_dict = auto_abstractor.summarize(document, abstractable_doc, similarity_filter)
 
     return result_dict["summarize_result"]
-
-
