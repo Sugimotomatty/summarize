43abfaf54ff9e545f3d9a148029717787ccf3eca
shung-ich <takahashi.shunichi.83a@st.kyoto-u.ac.jp>
2022-03-30 22:45:47 +0900

    txtファイル用のフォーム作成

--- app.py
+++ app.py
@@ -1,15 +1,24 @@
 from flask import Flask, render_template, request
-from wtforms import Form, FloatField, SubmitField, validators, ValidationError,StringField,TextAreaField
+from wtforms import Form, FloatField, SubmitField, validators, ValidationError,StringField,TextAreaField, FileField
 import numpy as np
 
 import summarize 
 
 app = Flask(__name__)
 
+class CheckORField(object):
+    def __init__(self, message):
+        self.message = message
+
+    def __call__(self, form, field):
+        if len(form['EnterSentence'].data) == 0 and form['upload_file'].data.split('.')[-1] != 'txt':
+            raise ValidationError(self.message)
+
 # Flaskとwtformsを使い、index.html側で表示させるフォームを構築する
 class EnterForm(Form):
-    EnterSentence = TextAreaField('要約したい文を貼り付け、または入力してください', [validators.InputRequired("この項目は入力必須です")])
-
+    # EnterSentence = TextAreaField('要約したい文を貼り付け、または入力してください', [validators.InputRequired("この項目は入力必須です")])
+    EnterSentence = TextAreaField('要約したい文を貼り付け、または入力してください', [CheckORField(message='文章の入力かtxtファイルのアップロードは必須です')])
+    upload_file = FileField()
 
     # html側で表示するsubmitボタンの表示
     submit = SubmitField("判定")
