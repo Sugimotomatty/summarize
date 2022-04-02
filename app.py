a6ffac077ca4b36d98f794fb90b0f378bd4f5e1b
ソラノトモコ <102491931+Solano-git@users.noreply.github.com>
2022-04-02 23:02:44 +0900

    Add files via upload

--- app.py
+++ app.py
@@ -1,18 +1,31 @@
 from flask import Flask, render_template, request, make_response
-from wtforms import Form, FloatField, SubmitField, validators, ValidationError,StringField,TextAreaField
+from wtforms import Form, FloatField, SubmitField, validators, ValidationError,StringField,TextAreaField, FileField
 import numpy as np
 
 import summarize 
 
-app = Flask(__name__,static_folder='./templates/images')
+app = Flask(__name__,static_folder='./templates')
+
+class CheckForm(object):
+    def __call__(self, form, field):
+        n_str = len(form['EnterSentence'].data)
+        extension = request.files['upload_file'].filename.split('.')[-1]
+        if n_str == 0:
+            if extension == '':
+                raise ValidationError('文の入力かtxtファイルのアップロードは必須です')
+            elif extension != 'txt':
+                raise ValidationError('.txtファイルをアップロードしてください')
+        else:
+            if extension != '':
+                raise ValidationError('文かtxtファイルいずれか一つを入力してください')
 
 # Flaskとwtformsを使い、index.html側で表示させるフォームを構築する
 class EnterForm(Form):
-    EnterSentence = TextAreaField('要約したい文を貼り付け、または入力してください', [validators.InputRequired("この項目は入力必須です")])
-
+    EnterSentence = TextAreaField('要約したい文を入力, もしくは.txtファイルをアップロードしてください', [CheckForm()])
+    upload_file = FileField()
 
     # html側で表示するsubmitボタンの表示
-    submit = SubmitField("判定")
+    submit = SubmitField("要約")
 
 x = ''
 
@@ -20,13 +33,15 @@ x = ''
 def predicts():
     form = EnterForm(request.form)
     if request.method == 'POST':
-        print(form.EnterSentence.data)
         if form.validate() == False:
             return render_template('index.html', form=form)
         else:            
-            EnterSentence = form.EnterSentence.data
+            if len(form['EnterSentence'].data) > 0:
+                sentence = form.EnterSentence.data
+            else:
+                sentence = request.files['upload_file'].read().decode('utf-8')
 
-            result = summarize.summa(EnterSentence) #ここでsummarizeファイルにおけるsumma関数を呼び出し
+            result = summarize.summa(sentence) #ここでsummarizeファイルにおけるsumma関数を呼び出し
 
             result1 =""
             for fragment in result:
